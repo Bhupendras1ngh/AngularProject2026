f14628255f817fc94497b2bbe7935083931428f3
Bhupendra <bhupendra@gamil.com>
2026-06-19 16:41:31 +0530

    version3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,40 +1,80 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { ProfileComponent } from './profile/profile.component';
-
+// import {signal} from 'rxjs'
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Login, ProfileComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  constructor(){
+    effect(()=>{
+      // console.log(this.userName())
+      if(this.count == 2) {this.signalDisplay = true; 
+        setTimeout(()=>{this.signalDisplay =false}, 2000)}
+      else this.signalDisplay =false;
+    })
+  }
   protected readonly title = signal('prelims2026');
   count = 0;
-  name ="";
-  displayname ="";
-  email ="";
+  userName = signal(0);
+  name = '';
+  displayname = '';
+  email = '';
+  display = false;
+  toggleDiv = true;
+  signalDisplay =false;
+  countSignal = signal(10);
+  users = [
+    { name: 'Sonu', age: 24, email: 'sonu@gmail.com' },
+    { name: 'golu', age: 26, email: 'golu@gmail.com' },
+    { name: 'raghav', age: 26, email: 'golu@gmail.com' },
+    { name: 'vipul', age: 26, email: 'vipul@gmail.com' },
+    { name: 'banti', age: 26, email: 'banti@gmail.com' },
+    { name: 'birju', age: 26, email: 'birju@gmail.com' },
+  ];
   // showName ="";
-  handleCount(val :string){
-    if(val =="minus"){
-      if( this.count >=1 ) this.count -=1;}
-    else if ( val =="add") this.count +=1;
-    else if (val =="reset") this.count =0;
+  updateSignal(){
+    this.userName.set(this.count +=1)
+    // this.signalDisplay =! this.signalDisplay;
+    // console.log(this.signalDisplay())
   }
-handleEvent(event : any){console.log('function called ', event);}
-getName(event :Event){
-  this.name =(event.target as HTMLInputElement).value
- console.log(name);
-}
-displayName(){ this.displayname = this.name;
-console.log(this.displayname)
-}
-setName(){
-  this.name ="Ravan";
-  this.displayname = this.name;
-}
-getEmail(event :string){console.log("get Email", event); this.email = event}
-// setEmail(event :string){console.log("set Email", event)}
-
+  handleCount(val: string) {
+    if (val == 'minus') {
+      if (this.count >= 1) this.count -= 1;
+    } else if (val == 'add') this.count += 1;
+    else if (val == 'reset') this.count = 0;
+  }
+  handleEvent(event: any) {
+    console.log('function called ', event);
+  }
+  getName(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+    console.log(name);
+  }
+  displayName() {
+    this.displayname = this.name;
+    console.log(this.displayname);
+  }
+  setName() {
+    this.name = 'Ram';
+    this.displayname = this.name;
+  }
+  getEmail(event: string) {
+    console.log('get Email', event);
+    this.email = event;
+  }
+  hideDiv(event: string) {
+    if (event == 'on') this.display = false;
+    else if (event == 'off') this.display = true;
+  }
+  toggle() {
+    this.display = !this.display;
+  }
+  toggleTwo() {
+    this.toggleDiv = !this.toggleDiv;
+  }
 }
